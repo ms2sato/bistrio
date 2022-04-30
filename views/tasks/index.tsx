3a98eb04dda06a7799b4f86588c1dcc48487c3d3
Masashi Sato <ms2.sato@gmail.com>
2022-04-30 13:17:33 +0900

    add suspense draft

--- views/tasks/index.tsx
+++ views/tasks/index.tsx
@@ -1,4 +1,4 @@
-import 'react'
+import { Suspense } from 'react'
 import { Task } from '../../server/entities/Task'
 
 type Prop = {
@@ -9,30 +9,79 @@ export function Index({ tasks }: Prop) {
   return (
     <>
       <h1>Task list</h1>
+      <Suspense fallback={<p>Loading...</p>}>
+        <MySuspend></MySuspend>
+      </Suspense>
       <a href="/tasks/build">create new task</a>
       <table>
-        <tr>
-          <th>ID</th>
-          <th>Done</th>
-          <th>Title</th>
-          <th>Description</th>
-          <th>Actions</th>
-        </tr>
-        {tasks.map((task) => (
-          <tr key={task.id}>
-            <td>{task.id}</td>
-            <td>{task.done ? 'closed' : <a href={`/tasks/${task.id}/done?_method=post`}>done</a>}</td>
-            <td>{task.title}</td>
-            <td>{task.description}</td>
-            <td>
-              <a href={`/tasks/${task.id}/edit`}>edit</a>&nbsp;|&nbsp;
-              <a href={`/tasks/${task.id}?_method=destory`}>delete</a>
-            </td>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Done</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Actions</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {tasks.map((task) => (
+            <tr key={task.id}>
+              <td>{task.id}</td>
+              <td>{task.done ? 'closed' : <a href={`/tasks/${task.id}/done?_method=post`}>done</a>}</td>
+              <td>{task.title}</td>
+              <td>{task.description}</td>
+              <td>
+                <a href={`/tasks/${task.id}/edit`}>edit</a>&nbsp;|&nbsp;
+                <a href={`/tasks/${task.id}?_method=destory`}>delete</a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </>
   )
 }
+
+let counter = 0
+let sleepTime = 0
+let data: { result: string } | undefined
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+async function fetchSomething(ms: number) {
+  await sleep(ms)
+  data = { result: `random: ${Math.random()}` }
+  return data
+}
+
+const MySuspend = () => {
+  counter++
+  if (data === undefined) {
+    sleepTime = 1000 * ((counter % 3) + 1)
+    throw fetchSomething(sleepTime)
+  }
+
+  const result = data.result
+  data = undefined
+  return (
+    <p>
+      Hello, world! {counter}, waited: {sleepTime}ms: {result}
+    </p>
+  )
+}
+
+// const MySuspend = () => {
+//   if (counter++ % 2 == 0) {
+//     sleepTime = 1000 * ((counter % 3) + 1)
+//     throw fetchSomething(sleepTime)
+//   }
+//   return (
+//     <p>
+//       Hello, world! {counter}, waited: {sleepTime}ms
+//     </p>
+//   )
+// }
 
 export default Index
