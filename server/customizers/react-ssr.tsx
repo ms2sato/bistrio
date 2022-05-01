d05cc2cb72ee66b947fee547db6096c662a161d4
Masashi Sato <ms2.sato@gmail.com>
2022-05-01 11:52:57 +0900

    refactor RenderSupport

--- server/customizers/react-ssr.tsx
+++ server/customizers/react-ssr.tsx
@@ -2,11 +2,66 @@ import { Application } from 'express'
 import { ActionContextCreator } from 'restrant2'
 import { actionContextCreatorFactory, NodeArrangeFunc, engine } from '../lib/restrant2-react-render'
 import { Layout } from '../../views/_layout'
+import { createContext, ReactNode, useState } from 'react'
+
+type Reader<T> = () => T
+type ReaderMap = Map<string, Reader<unknown>>
+
+// @see https://blog.logrocket.com/react-suspense-data-fetching/#data-fetching-approaches
+function suspendable<T>(promise: Promise<T>): Reader<T> {
+  let result: T
+  let err: Error
+  const suspender = promise.then(
+    (ret) => (result = ret),
+    (e: Error) => (err = e)
+  )
+  return () => {
+    if (result) return result
+    if (err) throw err
+    throw suspender
+  }
+}
+
+type RenderSupport = {
+  fetchJson: <T>(url: string, key?: string) => T
+}
+
+class RenderSupportImpl implements RenderSupport {
+  constructor(private readerMap: ReaderMap = new Map()) {}
+
+  fetchJson<T>(url: string, key: string = url): T {
+    console.log('### call request')
+
+    let reader: Reader<unknown> | undefined = this.readerMap.get(key)
+    if (!reader) {
+      console.log('undefined reader, start fetch')
+      reader = suspendable(
+        fetch(url).then((ret) => {
+          return ret.json()
+        })
+      )
+      this.readerMap.set(key, reader)
+    }
+    return (reader as Reader<T>)()
+  }
+}
+
+export const RenderSupportContext = createContext<RenderSupport>(new RenderSupportImpl())
 
 const arrange: NodeArrangeFunc = (Page, options) => {
   return (
-    <Layout>
+    <Wrapper>
       <Page {...options}></Page>
+    </Wrapper>
+  )
+}
+
+const Wrapper = ({ children }: { children: ReactNode }) => {
+  const [ctx] = useState(new RenderSupportImpl())
+
+  return (
+    <Layout>
+      <RenderSupportContext.Provider value={ctx}>{children}</RenderSupportContext.Provider>
     </Layout>
   )
 }
--- views/tasks/index.tsx
+++ views/tasks/index.tsx
@@ -1,124 +1,80 @@
-import { Suspense } from 'react'
+import { useContext, Suspense } from 'react'
 import { ActionContext } from 'restrant2'
-import fetch from 'node-fetch'
 import { Task } from '../../server/entities/Task'
+import { RenderSupportContext } from '../../server/customizers/react-ssr'
 
 type Prop = {
   tasks: Task[]
   ctx: ActionContext
 }
 
-export function Index({ tasks, ctx }: Prop) {
+export function Index(_prop: Prop) {
   return (
     <>
       <h1>Task list</h1>
       <Suspense fallback={<p>Loading...</p>}>
-        <MySuspend ctx={ctx}></MySuspend>
+        <MySuspend></MySuspend>
       </Suspense>
       <a href="/tasks/build">create new task</a>
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Done</th>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tasks.map((task) => (
-            <tr key={task.id}>
-              <td>{task.id}</td>
-              <td>{task.done ? 'closed' : <a href={`/tasks/${task.id}/done?_method=post`}>done</a>}</td>
-              <td>{task.title}</td>
-              <td>{task.description}</td>
-              <td>
-                <a href={`/tasks/${task.id}/edit`}>edit</a>&nbsp;|&nbsp;
-                <a href={`/tasks/${task.id}?_method=destory`}>delete</a>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <Suspense fallback={<p>Loading...</p>}>
+        <TaskTable></TaskTable>
+      </Suspense>
     </>
   )
 }
 
-function request(ctx: ActionContext, url: string) {
-  console.log('### call request')
-  let readerMap: ReaderMap | undefined = requestMap.get(ctx)
-  if (!readerMap) {
-    readerMap = new Map()
-    requestMap.set(ctx, readerMap)
-  }
+// TODO: define params and JsonResponder
+type TaskRes = { status: string; data: { task: Task } }
+type TasksRes = { status: string; data: { tasks: Task[] } }
 
-  let reader: Reader | undefined = readerMap.get(url)
-  if (!reader) {
-    console.log('undefined reader, start fetch')
-    reader = wrap(
-      fetch(url).then((ret) => {
-        return ret.json()
-      })
-    )
-    readerMap.set(url, reader)
-  }
-  return reader
-}
+const MySuspend = () => {
+  const ctx = useContext(RenderSupportContext)
 
-// @see https://blog.logrocket.com/react-suspense-data-fetching/#data-fetching-approaches
-function wrap<T>(promise: Promise<T>): { read: () => T } {
-  let status = 'pending'
-  let result: T
-  let err: Error
-  const suspender = promise.then(
-    (ret) => {
-      status = 'success'
-      result = ret
-    },
-    (e: Error) => {
-      status = 'error'
-      err = e
-    }
-  )
-  return {
-    read: () => {
-      switch (status) {
-        case 'pending':
-          console.log('read: pending')
-          throw suspender
-        case 'error':
-          console.log('read: err', err)
-          throw err
-        default:
-          return result
-      }
-    },
-  }
-}
-
-type Reader = { read: () => any }
-type URL = string
-type ReaderMap = Map<URL, Reader>
-type ActionContextReaderMap = Map<ActionContext, ReaderMap>
-
-const requestMap: ActionContextReaderMap = new Map()
-
-const MySuspend = ({ ctx }: { ctx: ActionContext }) => {
-  const reader = request(ctx, 'http://localhost:3000/tasks/1/edit.json')
-  const res = reader.read() as Record<string, any>
+  const res = ctx.fetchJson<TaskRes>('http://localhost:3000/tasks/1/edit.json')
   console.log(res)
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  const task = res.data.task as Task
+  const task = res.data.task
   console.log(task)
   return (
     <div>
-      <h3>request on server</h3>
-      <div>
-        {task.title}, {task.description}
-      </div>
+      {task.title}, {task.description}
     </div>
+  )
+}
+
+const TaskTable = () => {
+  const ctx = useContext(RenderSupportContext)
+
+  const res = ctx.fetchJson<TasksRes>('http://localhost:3000/tasks.json')
+  console.log(res)
+
+  const tasks = res.data.tasks
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Done</th>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tasks.map((task) => (
+          <tr key={task.id}>
+            <td>{task.id}</td>
+            <td>{task.done ? 'closed' : <a href={`/tasks/${task.id}/done?_method=post`}>done</a>}</td>
+            <td>{task.title}</td>
+            <td>{task.description}</td>
+            <td>
+              <a href={`/tasks/${task.id}/edit`}>edit</a>&nbsp;|&nbsp;
+              <a href={`/tasks/${task.id}?_method=destory`}>delete</a>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
 
