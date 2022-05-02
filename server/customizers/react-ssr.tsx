168ead50080c056859a5a19fd4a5e775e81861ba
Masashi Sato <ms2.sato@gmail.com>
2022-05-02 09:50:59 +0900

    reafactor

--- server/customizers/react-ssr.tsx
+++ server/customizers/react-ssr.tsx
@@ -1,52 +1,16 @@
 import { Application } from 'express'
 import { ActionContextCreator } from 'restrant2'
-import { actionContextCreatorFactory, NodeArrangeFunc, engine } from '../lib/restrant2-react-render'
+import {
+  buildActionContextCreator,
+  NodeArrangeFunc,
+  engine,
+  RenderSupport,
+  createRenderSupport,
+} from '../lib/restrant2-react-render'
 import { Layout } from '../../views/_layout'
 import { createContext, ReactNode, useState } from 'react'
 
-type Reader<T> = () => T
-type ReaderMap = Map<string, Reader<unknown>>
-
-// @see https://blog.logrocket.com/react-suspense-data-fetching/#data-fetching-approaches
-function suspendable<T>(promise: Promise<T>): Reader<T> {
-  let result: T
-  let err: Error
-  const suspender = promise.then(
-    (ret) => (result = ret),
-    (e: Error) => (err = e)
-  )
-  return () => {
-    if (result) return result
-    if (err) throw err
-    throw suspender
-  }
-}
-
-type RenderSupport = {
-  fetchJson: <T>(url: string, key?: string) => T
-}
-
-class RenderSupportImpl implements RenderSupport {
-  constructor(private readerMap: ReaderMap = new Map()) {}
-
-  fetchJson<T>(url: string, key: string = url): T {
-    console.log('### call request')
-
-    let reader: Reader<unknown> | undefined = this.readerMap.get(key)
-    if (!reader) {
-      console.log('undefined reader, start fetch')
-      reader = suspendable(
-        fetch(url).then((ret) => {
-          return ret.json()
-        })
-      )
-      this.readerMap.set(key, reader)
-    }
-    return (reader as Reader<T>)()
-  }
-}
-
-export const RenderSupportContext = createContext<RenderSupport>(new RenderSupportImpl())
+export const RenderSupportContext = createContext<RenderSupport>(createRenderSupport())
 
 const arrange: NodeArrangeFunc = (Page, options) => {
   return (
@@ -57,7 +21,7 @@ const arrange: NodeArrangeFunc = (Page, options) => {
 }
 
 const Wrapper = ({ children }: { children: ReactNode }) => {
-  const [ctx] = useState(new RenderSupportImpl())
+  const [ctx] = useState(createRenderSupport())
 
   return (
     <Layout>
@@ -73,7 +37,7 @@ export const useTsxView = (app: Application, viewRoot: string) => {
   app.set('views', viewRoot)
   app.set('view engine', 'tsx')
 
-  ctxCreator = actionContextCreatorFactory(viewRoot, arrange, '')
+  ctxCreator = buildActionContextCreator(viewRoot, arrange, '')
 }
 
 export const createActionContext: ActionContextCreator = (props) => {
--- server/lib/restrant2-react-render.ts
+++ server/lib/restrant2-react-render.ts
@@ -31,13 +31,13 @@ export const createRenderFunc = (arrange: NodeArrangeFunc, viewRoot: string, fai
   return newRender
 }
 
-export type ActionContextCreatorFactory = (
+export type BuildActionContextCreator = (
   viewRoot: string,
   arrange: NodeArrangeFunc,
   failText: string
 ) => ActionContextCreator
 
-export const actionContextCreatorFactory: ActionContextCreatorFactory = (
+export const buildActionContextCreator: BuildActionContextCreator = (
   viewRoot: string,
   arrange: NodeArrangeFunc,
   failText = ''
@@ -46,5 +46,50 @@ export const actionContextCreatorFactory: ActionContextCreatorFactory = (
     const ctx = createDefaultActionContext(props)
     ctx.render = createRenderFunc(arrange, viewRoot, failText)
     return ctx
+  }
+}
+
+// -----
+// for SSR and CSR
+
+export type Reader<T> = () => T
+type ReaderMap = Map<string, Reader<unknown>>
+
+// @see https://blog.logrocket.com/react-suspense-data-fetching/#data-fetching-approaches
+export function suspendable<T>(promise: Promise<T>): Reader<T> {
+  let result: T
+  let err: Error
+  const suspender = promise.then(
+    (ret) => (result = ret),
+    (e: Error) => (err = e)
+  )
+  return () => {
+    if (result) return result
+    if (err) throw err
+    throw suspender
+  }
+}
+
+export type RenderSupport = {
+  fetchJson: <T>(url: string, key?: string) => T
+}
+
+export const createRenderSupport = () => new RenderSupportImpl()
+
+class RenderSupportImpl implements RenderSupport {
+  constructor(private readerMap: ReaderMap = new Map()) {}
+
+  fetchJson<T>(url: string, key: string = url): T {
+    let reader: Reader<unknown> | undefined = this.readerMap.get(key)
+    if (!reader) {
+      console.log('undefined reader, start fetch')
+      reader = suspendable(
+        fetch(url).then((ret) => {
+          return ret.json()
+        })
+      )
+      this.readerMap.set(key, reader)
+    }
+    return (reader as Reader<T>)()
   }
 }
