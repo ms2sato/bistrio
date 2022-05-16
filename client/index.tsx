340d37f5f129b096652c811f6984e8d0d1c21fc1
Masashi Sato <ms2.sato@gmail.com>
2022-05-16 09:15:50 +0900

    install router

--- client/index.tsx
+++ client/index.tsx
@@ -1,6 +1,17 @@
 import * as React from 'react'
 import { hydrateRoot } from 'react-dom/client'
 import { Build } from '../views/tasks/build'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+const root = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/tasks/build" element={<Build task={{ title: '', description: '' }} />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 const container = document.getElementById('app')
-hydrateRoot(container, <Build task={{ title: '', description: '' }} />)
+hydrateRoot(container, root())
