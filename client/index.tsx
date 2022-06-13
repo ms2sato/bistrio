d45050c4502b34c7d2fe0fe08602e8c759a56a10
Masashi Sato <ms2.sato@gmail.com>
2022-06-13 09:41:59 +0900

    refactor lib/client

--- client/index.tsx
+++ client/index.tsx
@@ -1,46 +1,15 @@
 import * as React from 'react'
 import { useState } from 'react'
-import { hydrateRoot } from 'react-dom/client'
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Resource, NamedResources } from 'restrant2/client'
-import { LocaleSelector, Localizer } from '../lib/locale'
+import { hydrateRoot } from 'react-dom/client'
+import { LocaleSelector } from '../lib/locale'
 import { initLocale } from '../lib/localizer'
-import { RenderSupport, suspense } from '../lib/render-support'
+import { setup, App } from '../lib/client'
+
 import { views } from '../views'
-import { ClientGenretateRouter, ClientGenretateRouterCore } from '../lib/client-stub-router'
 import { routes } from '../routes'
 import { N2R } from '../_types'
-
-class ClientRenderSupport<RS extends NamedResources> implements RenderSupport<RS> {
-  private suspense
-  constructor(private localeSelector?: LocaleSelector) {
-    this.suspense = suspense()
-  }
-
-  getLocalizer(): Localizer {
-    return this.localeSelector.select(navigator.language)
-  }
-
-  fetchJson<T>(url: string, key?: string): T {
-    return this.suspense.fetchJson(url, key)
-  }
-
-  resourceOf(name: string): Resource {
-    return core.resourceNameToInfo.get(name).resource
-  }
-
-  resources(): RS {
-    const ret: { [key: string]: Resource } = {}
-    for (const [key, info] of core.resourceNameToInfo.entries()) {
-      ret[key] = info.resource
-    }
-    return ret as RS
-  }
-
-  suspend<T>(asyncProcess: () => Promise<T>, key: string): T {
-    return this.suspense.suspend(asyncProcess, key)
-  }
-}
 
 const root = async () => {
   const localeSelector = await initLocale()
@@ -49,11 +18,11 @@ const root = async () => {
 }
 
 const Root = ({ localeSelector }: { localeSelector: LocaleSelector }) => {
-  const [renderSupport] = useState(new ClientRenderSupport<N2R>(localeSelector))
+  const [renderSupport] = useState(instance.createRenderSupport(localeSelector))
   return (
     <BrowserRouter>
       <Routes>
-        {Array.from(core.pathToPage).map(([path, Page]) => {
+        {Array.from(instance.pathToPage()).map(([path, Page]) => {
           console.debug('<Route>', path, Page)
           return <Route key={path} path={path} element={<Page rs={renderSupport} />}></Route>
         })}
@@ -62,18 +31,18 @@ const Root = ({ localeSelector }: { localeSelector: LocaleSelector }) => {
   )
 }
 
-let core: ClientGenretateRouterCore<N2R>
+let instance: App<N2R>
 
-const hydrate = async () => {
-  const cgr = new ClientGenretateRouter<N2R>(views)
-  routes(cgr)
-  await cgr.build()
-  core = cgr.getCore()
-
+const boot = async () => {
+  instance = await setup<N2R>(routes, views)
   const container = document.getElementById('app')
+  if (!container) {
+    throw new Error('#app not found')
+  }
+
   hydrateRoot(container, await root())
 }
 
-hydrate().catch((err) => {
+boot().catch((err) => {
   console.error(err)
 })
---
+++ lib/client.ts
@@ -0,0 +1,65 @@
+import { Resource, NamedResources, Router } from 'restrant2/client'
+import { LocaleSelector, Localizer } from './locale'
+import { PageNode, RenderSupport, suspense } from './render-support'
+import { ClientGenretateRouter, ClientGenretateRouterCore, ResourceInfo, ViewDescriptor } from './client-stub-router'
+
+class ClientRenderSupport<RS extends NamedResources> implements RenderSupport<RS> {
+  private suspense
+
+  constructor(private core: ClientGenretateRouterCore<RS>, private localeSelector: LocaleSelector) {
+    this.suspense = suspense()
+  }
+
+  getLocalizer(): Localizer {
+    return this.localeSelector.select(navigator.language)
+  }
+
+  fetchJson<T>(url: string, key?: string): T {
+    return this.suspense.fetchJson(url, key)
+  }
+
+  resourceOf(name: string): Resource {
+    const info: ResourceInfo | undefined = this.core.resourceNameToInfo.get(name)
+    if (!info) {
+      throw new Error(`resource info not found: ${name}`)
+    }
+    return info.resource
+  }
+
+  resources(): RS {
+    const ret: { [name: string]: Resource } = {}
+    this.core.resourceNameToInfo.forEach((info, name) => {
+      ret[name] = info.resource
+    })
+    return ret as RS
+  }
+
+  suspend<T>(asyncProcess: () => Promise<T>, key: string): T {
+    return this.suspense.suspend(asyncProcess, key)
+  }
+}
+
+export type App<RS extends NamedResources> = {
+  createRenderSupport: (localeSelector: LocaleSelector) => ClientRenderSupport<RS>
+  pathToPage: () => Map<string, PageNode<RS>>
+}
+
+export async function setup<RS extends NamedResources>(
+  routes: (router: Router) => void,
+  views: ViewDescriptor<RS>
+): Promise<App<RS>> {
+  const cgr = new ClientGenretateRouter<RS>(views)
+  routes(cgr)
+  await cgr.build()
+  const core = cgr.getCore()
+
+  return {
+    createRenderSupport(localeSelector: LocaleSelector): ClientRenderSupport<RS> {
+      return new ClientRenderSupport<RS>(core, localeSelector)
+    },
+
+    pathToPage() {
+      return core.pathToPage
+    },
+  }
+}
