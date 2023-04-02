7d8b8d3363b892d6139b8e7297bc18eab345e19e
Masashi Sato <ms2.sato@gmail.com>
2023-04-02 19:03:35 +0900

    js build with content hash

--- example/tasks/server/customizers/render-support.tsx
+++ example/tasks/server/customizers/render-support.tsx
@@ -5,15 +5,15 @@ import { StaticRouter } from 'react-router-dom/server'
 import { ConstructViewFunc, initBistrioReactView } from 'bistrio'
 
 import { N2R } from '@bistrio/routes/all'
-import { Layout } from '../../isomorphic/components/Layout'
+import { Layout } from './Layout'
+import versions from '@bistrio/versions.json'
 
 const { Wrapper } = initBistrioReactView<N2R>()
 
 export const constructView: ConstructViewFunc = (Page, hydrate, options, ctx) => {
   // This is sample impl, changing js for any roles
   const script = ctx.query['admin'] == 'true' ? ['vendors', 'admin'] : ['vendors', 'main']
-
-  const props = { hydrate, script }
+  const props = { hydrate, script, versions }
   return (
     <Wrapper ctx={ctx}>
       <Layout props={props}>
