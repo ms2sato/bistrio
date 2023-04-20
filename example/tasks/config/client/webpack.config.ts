1daa97bd22866ae989c6ebc72c66f11139d8fc15
Masashi Sato <ms2.sato@gmail.com>
2023-04-20 18:52:00 +0900

    default optimize config to lib

--- example/tasks/config/client/webpack.config.ts
+++ example/tasks/config/client/webpack.config.ts
@@ -1,91 +1,22 @@
 import path from 'path'
-import { writeFile, chmod } from 'fs/promises'
-import { existsSync, mkdirSync } from 'fs'
-import webpack from 'webpack'
 import { entries } from '../../isomorphic/config'
 import { generateWebpackCoonfig } from 'bistrio'
 
-const publicJsPath = '/js/'
+const baseDir = path.resolve(__dirname, '../..')
 
-const baseDir = path.join(__dirname, '../..')
-const staticDir = path.resolve(baseDir, 'dist', 'public')
-
-let generateWebpackCoonfigParams
-if (process.env.NODE_ENV !== 'development') {
-  generateWebpackCoonfigParams = {
-    entries,
-    baseDir,
-    generateEntry: () => {
-      const entry = Object.keys(entries).reduce<webpack.EntryObject>((obj, name) => {
-        obj[name] = `./.bistrio/routes/${name}/_entry.ts`
-        return obj
-      }, {})
-      return entry
-    },
-  }
-} else {
-  generateWebpackCoonfigParams = {
-    entries,
-    baseDir,
-  }
+const generateWebpackCoonfigParams = {
+  entries,
+  baseDir,
+  // generateEntry: () => {
+  //   const entry = Object.keys(entries).reduce<webpack.EntryObject>((obj, name) => {
+  //     obj[name] = path.resolve(buildDir, `routes/${name}/_entry.ts`)
+  //     return obj
+  //   }, {})
+  //   return entry
+  // },
 }
 
-const pluginName = 'URLMapPlugin'
-class URLMapPlugin {
-  apply(compiler: webpack.Compiler) {
-    compiler.hooks.emit.tapAsync(pluginName, async (compilation, callback) => {
-      try {
-        const jsMap: Record<string, string> = {}
-
-        compilation.chunks.forEach((chunk) => {
-          chunk.files.forEach((file) => {
-            if (file.endsWith('.js')) {
-              jsMap[chunk.name] = file
-            }
-          })
-        })
-
-        const data = { files: { js: jsMap } }
-        const filename = path.resolve(baseDir, '.bistrio', 'versions.json')
-        await writeFile(filename, JSON.stringify(data, null, 2), { flag: 'w' })
-        await chmod(filename, 0o666)
-
-        callback()
-      } catch (err) {
-        callback(err as Error)
-      }
-    })
-  }
-}
 
 const webpackConfig = generateWebpackCoonfig(generateWebpackCoonfigParams)
-if (process.env.NODE_ENV === 'development') {
-  webpackConfig.output = {
-    ...webpackConfig.output,
-    publicPath: publicJsPath,
-  }
-} else {
-  const publicJsFullPath = path.join(staticDir, publicJsPath)
-  if (!existsSync(publicJsFullPath)) {
-    mkdirSync(publicJsFullPath, { recursive: true })
-  }
-  webpackConfig.output = {
-    ...webpackConfig.output,
-    path: publicJsFullPath,
-    filename: '[name].[contenthash].bundle.js',
-  }
-}
-webpackConfig.plugins = [new URLMapPlugin()]
-webpackConfig.optimization = {
-  splitChunks: {
-    chunks: 'initial',
-    cacheGroups: {
-      default: {
-        name: 'vendors',
-        reuseExistingChunk: true,
-      },
-    },
-  },
-}
 
 export default webpackConfig
--- example/tasks/server/customizers/Layout.tsx
+++ example/tasks/server/customizers/Layout.tsx
@@ -14,18 +14,26 @@ type GlobalProps = {
 }
 
 function Scripts({ props }: { props: GlobalProps }) {
+  const sharedPrefix = 'shared--'
+  const sharedScriptEntries = Object.entries(props.versions.files.js).filter(([key, _value]) =>
+    key.startsWith(sharedPrefix)
+  )
+  const sharedScripts = sharedScriptEntries.map((entries) => entries[1])
+
   const jsRoot = '/js' // TODO: shared
-  let scripts
-  if (process.env.NODE_ENV === 'development') {
+  let scripts: string[]
+  // if (process.env.NODE_ENV === 'development') {
+  //   scripts = Array.isArray(props.script)
+  //     ? props.script.map((js) => path.resolve(jsRoot, `${js}.js`))
+  //     : [path.resolve(jsRoot, props.script)]
+  // } else {
     scripts = Array.isArray(props.script)
-      ? props.script.map((js) => path.resolve(jsRoot, `${js}.js`))
-      : [path.resolve(jsRoot, props.script)]
-  } else {
-    scripts = Array.isArray(props.script)
-      ? props.script.map((js) => props.versions.files.js[js])
+      ? props.script.map(js => props.versions.files.js[js])
       : [props.versions.files.js[props.script]]
-  }
-  return <>{props.hydrate && scripts.map((js) => <script key={js} src={`/js/${js}`} defer></script>)}</>
+  // }
+
+  scripts = [...sharedScripts, ...scripts]
+  return <>{props.hydrate && scripts.map((js) => <script key={js} src={path.resolve(jsRoot, js)} defer></script>)}</>
 }
 
 export function Layout({ children, props }: { children: ReactNode; props: GlobalProps }) {
