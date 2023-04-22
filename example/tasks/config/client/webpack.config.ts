01afc6978653627af738ae4cdc1990c81f293bcd
Masashi Sato <ms2.sato@gmail.com>
2023-04-22 15:52:16 +0900

    refactor

--- example/tasks/config/client/webpack.config.ts
+++ example/tasks/config/client/webpack.config.ts
@@ -1,22 +1,12 @@
 import path from 'path'
-import { entries } from '../../isomorphic/config'
-import { generateWebpackCoonfig } from 'bistrio'
+import { entriesConfig } from '../../isomorphic/config'
+import { generateWebpackConfig } from 'bistrio'
 
 const baseDir = path.resolve(__dirname, '../..')
 
-const generateWebpackCoonfigParams = {
-  entries,
+const webpackConfig = generateWebpackConfig({
+  entriesConfig,
   baseDir,
-  // generateEntry: () => {
-  //   const entry = Object.keys(entries).reduce<webpack.EntryObject>((obj, name) => {
-  //     obj[name] = path.resolve(buildDir, `routes/${name}/_entry.ts`)
-  //     return obj
-  //   }, {})
-  //   return entry
-  // },
-}
-
-
-const webpackConfig = generateWebpackCoonfig(generateWebpackCoonfigParams)
+})
 
 export default webpackConfig
