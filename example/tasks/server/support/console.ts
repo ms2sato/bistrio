aad669a64c47bafb28e6b7d00ee9a0e6f9f5a944
Masashi Sato <ms2.sato@gmail.com>
2023-05-20 16:38:01 +0900

    pull up loadResources

--- example/tasks/server/support/console.ts
+++ example/tasks/server/support/console.ts
@@ -1,32 +1,9 @@
-// for bin/console
-// add modules for REPL
+// for REPL
+import { support } from 'bistrio'
 import { routes } from '../../isomorphic/routes/all'
-import {
-  nullRouterSupport,
-  Resource,
-  ResourceHolderCreateRouter,
-  Router,
-  RouterSupport,
-  ServerRouterConfigCustom,
-} from 'bistrio'
 import { config } from '../config/server'
 import { Middlewares } from '../../isomorphic/routes/middlewares'
 
-const main = async (
-  serverRouterConfig: ServerRouterConfigCustom,
-  routes: (router: Router, support: RouterSupport<Middlewares>) => void
-) => {
-  const resources = {} as Record<string, Resource>
-
-  // TODO: fix any
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  ;(global as any).resources = resources
-
-  const router = new ResourceHolderCreateRouter(resources, serverRouterConfig, '/')
-  routes(router, nullRouterSupport as RouterSupport<Middlewares>)
-  await router.build()
-}
-
-main(config(), routes).catch((err: Error) => {
+support.loadResources<Middlewares>(config(), routes).catch((err: Error) => {
   console.error(`console error: ${err.message}`)
 })
