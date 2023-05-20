b82ee7b6b061342f1180ff8fbb1549d0e6621d56
Masashi Sato <ms2.sato@gmail.com>
2023-05-20 17:34:51 +0900

    move to ResourceHolderCreateRouter from restrant2

--- src/lib/support/console-load-resources.ts
+++ src/lib/support/console-load-resources.ts
@@ -1,12 +1,79 @@
+import path from 'path'
+import debug from 'debug'
 import {
+  BasicRouter,
+  createLocalResourceProxy,
+  HandlerBuildRunner,
+  importAndSetup,
   Middlewares,
   nullRouterSupport,
   Resource,
-  ResourceHolderCreateRouter,
+  ResourceSupport,
+  RouteConfig,
   Router,
+  RouterCore,
+  RouterOptions,
   RouterSupport,
   ServerRouterConfigCustom,
 } from '../../'
+
+const log = debug('bistrio')
+const debugLog = log.extend('console')
+
+class ResourceHolderCreateRouter extends BasicRouter {
+  private nameToPath: Record<string, string> = {}
+
+  constructor(
+    private resourcesHolder: Record<string, Resource>,
+    serverRouterConfig: ServerRouterConfigCustom,
+    httpPath = '/',
+    protected readonly routerCore: RouterCore = { handlerBuildRunners: [], nameToResource: new Map() },
+    private routerOptions: RouterOptions = { hydrate: false }
+  ) {
+    super(serverRouterConfig, httpPath, routerCore)
+  }
+
+  sub(rpath: string) {
+    debugLog('sub: %s', rpath)
+    return new ResourceHolderCreateRouter(
+      this.resourcesHolder,
+      this.serverRouterConfig,
+      path.join(this.httpPath, rpath),
+      this.routerCore,
+      { ...this.routerOptions }
+    )
+  }
+
+  options(value: RouterOptions) {
+    this.routerOptions = value
+    return this
+  }
+
+  protected createHandlerBuildRunner(rpath: string, routeConfig: RouteConfig): HandlerBuildRunner {
+    debugLog('createHandlerBuildRunner: %s', rpath)
+    const fileRoot = this.serverRouterConfig.baseDir
+    return async () => {
+      debugLog('%s', rpath)
+      const resourcePath = this.getResourcePath(rpath)
+      const resourceSupport = new ResourceSupport(fileRoot)
+      const resource = await importAndSetup<ResourceSupport, Resource>(
+        fileRoot,
+        resourcePath,
+        resourceSupport,
+        routeConfig
+      )
+
+      const resourceProxy = createLocalResourceProxy(routeConfig, resource)
+      const name = routeConfig.name
+      if (this.resourcesHolder[name]) {
+        throw new Error(`Duplicated Resource name: ${name}; path: ${resourcePath}, with: ${this.nameToPath[name]}`)
+      }
+
+      this.resourcesHolder[name] = resourceProxy
+      this.nameToPath[name] = resourcePath
+    }
+  }
+}
 
 export async function loadResources<M extends Middlewares>(
   serverRouterConfig: ServerRouterConfigCustom,
