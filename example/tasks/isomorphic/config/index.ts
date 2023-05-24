1dd465c30f3d9f1999e6699823b49b992a40db7c
Masashi Sato <ms2.sato@gmail.com>
2023-05-24 21:22:51 +0900

    arrange clientConfig, Fetcher

--- example/tasks/isomorphic/config/index.ts
+++ example/tasks/isomorphic/config/index.ts
@@ -1,16 +1,7 @@
 import React from 'react'
-import { EntriesConfig } from 'bistrio/client'
+import { ClientConfig, EntriesConfig, defaultClientConfig } from 'bistrio/client'
 import { routes as mainRoutes } from '../routes/main'
 import { routes as adminRoutes } from '../routes/admin'
-
-// // basic configuration
-// import { Actions, ClientRouterConfig } from "bistrio/client"
-// export const config = ():Partial<ClientRouterConfig> => {
-//   return {
-//     host: window.location.origin,
-//     constructConfig: Actions.defaultConstructConfig(),
-//   }
-// }
 
 const getContainerElement = () => {
   const el = document.getElementById('app')
@@ -32,3 +23,5 @@ export const entriesConfig: EntriesConfig = {
   main: { routes: mainRoutes, getContainerElement, pageLoadFunc },
   admin: { routes: adminRoutes, getContainerElement, pageLoadFunc },
 }
+
+export const clientConfig: ClientConfig = defaultClientConfig()
