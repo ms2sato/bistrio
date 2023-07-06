cdb08bf7a715089ced76b6e2df383da9677498b7
Masashi Sato <ms2.sato@gmail.com>
2023-07-07 06:58:41 +0900

    example: add error boundary for RoutesWrapper

--- example/tasks/isomorphic/components/RoutesWrapper.tsx
+++ example/tasks/isomorphic/components/RoutesWrapper.tsx
@@ -1,5 +1,19 @@
 import { Suspense, ReactNode } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
+
+function PageNotFound() {
+  return (
+    <div>
+      <h2>Page chunk not found</h2>
+      <p>Reload please.</p>
+    </div>
+  )
+}
 
 export function RoutesWrapper({ children }: { children: ReactNode }) {
-  return <Suspense fallback="<p>Loading...</p>">{children}</Suspense>
+  return (
+    <ErrorBoundary fallback={<PageNotFound></PageNotFound>}>
+      <Suspense fallback="<p>Loading...</p>">{children}</Suspense>
+    </ErrorBoundary>
+  )
 }
