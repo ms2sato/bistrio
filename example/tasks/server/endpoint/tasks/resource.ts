cdee91ec31350bd6552c2fcdd19aef260f81470d
Masashi Sato <ms2.sato@gmail.com>
2023-07-07 07:18:16 +0900

    remove actions for page only resource

--- example/tasks/server/endpoint/tasks/resource.ts
+++ example/tasks/server/endpoint/tasks/resource.ts
@@ -1,29 +1,3 @@
-import { defineResource, IdNumberParams } from 'bistrio'
-import { Prisma, Task } from '@prisma/client'
-import { TaskCreateParams, TaskUpdateParams } from '@isomorphic/params'
-import { createPrismaEasyDataAccessor, getPrismaCilent } from '@server/lib/prisma-util'
+import { defineResource } from 'bistrio'
 
-const prisma = getPrismaCilent()
-
-const accessor = createPrismaEasyDataAccessor<Task, IdNumberParams, Prisma.TaskCreateInput, Prisma.TaskUpdateInput>(
-  prisma.task,
-  'id'
-)
-
-export default defineResource((_support, _options) => ({
-  create: async (params: TaskCreateParams) => {
-    return accessor.create({ ...params, done: false })
-  },
-
-  update: async (params: TaskUpdateParams) => {
-    return accessor.update(params)
-  },
-
-  destroy: async (params: IdNumberParams) => {
-    return accessor.destroy(params)
-  },
-
-  done: async ({ id }: IdNumberParams) => {
-    await prisma.task.update({ where: { id }, data: { done: true } })
-  },
-}))
+export default defineResource((_support, _options) => ({}))
