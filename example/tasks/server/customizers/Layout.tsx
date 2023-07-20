1a7579cdde0b26e8840da8a0b0ca477b85f64bce
Masashi Sato <ms2.sato@gmail.com>
2023-07-21 00:03:40 +0900

    <Liveroad /> pullup to lib

--- example/tasks/server/customizers/Layout.tsx
+++ example/tasks/server/customizers/Layout.tsx
@@ -1,25 +1,5 @@
 import { ReactNode } from 'react'
-import { ScriptProps, Scripts } from 'bistrio'
-
-function Livereload() {
-  if (process.env.NODE_ENV === 'production') {
-    return <></>
-  }
-
-  const port = process.env.LIVERELOAD_PORT || 35729
-  const origin = process.env.LIVERELOAD_ORIGIN || `http://' + (location.host || 'localhost').split(':')[0] + ':${port}`
-  const src = `${origin}/livereload.js?snipver=1`
-  const livereloadScript = `document.write('<script src="${src}" defer></' + 'script>')`
-
-  return (
-    <script
-      defer
-      dangerouslySetInnerHTML={{
-        __html: livereloadScript,
-      }}
-    />
-  )
-}
+import { Livereload, ScriptProps, Scripts } from 'bistrio'
 
 export function Layout({ children, ...props }: { children: ReactNode } & ScriptProps) {
   return (
---
+++ src/lib/Liveroad.tsx
@@ -0,0 +1,19 @@
+export function Livereload() {
+  if (process.env.NODE_ENV === 'production') {
+    return <></>
+  }
+
+  const port = process.env.LIVERELOAD_PORT || 35729
+  const origin = process.env.LIVERELOAD_ORIGIN || `http://' + (location.host || 'localhost').split(':')[0] + ':${port}`
+  const src = `${origin}/livereload.js?snipver=1`
+  const livereloadScript = `document.write('<script src="${src}" defer></' + 'script>')`
+
+  return (
+    <script
+      defer
+      dangerouslySetInnerHTML={{
+        __html: livereloadScript,
+      }}
+    />
+  )
+}
