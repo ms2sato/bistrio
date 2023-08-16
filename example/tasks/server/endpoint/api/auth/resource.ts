9fbd204cee007f2972e3ee5f7646f970860e96fe
Masashi SATO <ms2.sato@gmail.com>
2023-08-17 02:58:34 +0900

    example: fix lint

--- example/tasks/server/endpoint/api/auth/resource.ts
+++ example/tasks/server/endpoint/api/auth/resource.ts
@@ -4,6 +4,9 @@ import { User } from '@/isomorphic/params'
 import { SessionCreateParams } from '@/isomorphic/params'
 
 export default defineResource((_support, _options) => ({
+  create: (_params: SessionCreateParams): Promise<User> => {
+    throw new Error('override by adapter')
+  },
   verify: (_params: SessionCreateParams): Promise<User> => {
     throw new Error('override by adapter')
   },
