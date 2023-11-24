5cb369fafef238b84f9b8c7442aae009ae9611d2
Masashi SATO <ms2.sato@gmail.com>
2023-11-24 10:05:09 +0900

    draft load Page with webpack

--- example/tasks/server/config/server.ts
+++ example/tasks/server/config/server.ts
@@ -1,12 +1,23 @@
 import { resolve, relative } from 'path'
 import { ServerRouterConfig, initServerRouterConfig, FileNotFoundError } from 'bistrio'
 import { createActionOptions } from '../customizers/index'
-import { pageLoadFunc } from '@universal/config'
+import { ComponentType, lazy } from 'react'
 
 async function importLocal(this: ServerRouterConfig, filePath: string) {
   try {
     const relativePath = relative(resolve(this.baseDir, 'resources'), filePath)
     return require(`../resources/${relativePath}`)
+  } catch (err) {
+    throw new FileNotFoundError(filePath, { cause: err })
+  }
+}
+
+function pageLoadFunc(filePath: string) {
+  try {
+    return lazy(() => {
+      const { Page } = require(`../../universal/pages${filePath}`)
+      return Promise.resolve({ default: Page as ComponentType<any> })
+    })
   } catch (err) {
     throw new FileNotFoundError(filePath, { cause: err })
   }
