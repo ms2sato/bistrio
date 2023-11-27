218212e672c497494a1628d898c2966559e41069
Masashi SATO <ms2.sato@gmail.com>
2023-11-27 20:27:11 +0900

    fix npm run console

--- example/tasks/bin/console.js
+++
@@ -1,8 +0,0 @@
-// for REPL
-const { support } = require('bistrio')
-const { routes } = require('../dist/universal/routes/all.js')
-const { serverRouterConfig } = require('../dist/server/config/server.js')
-
-support.loadResources(serverRouterConfig(), routes).catch((err) => {
-  console.error(`console error: ${err.message}`)
-})
