2451f3dc0bbbee3f0e2a9308948eeeb0866635f1
Masashi SATO <ms2.sato@gmail.com>
2023-11-29 11:35:59 +0900

    fix webpack config process optimize

--- example/tasks/config/server/webpack.config.ts
+++ example/tasks/config/server/webpack.config.ts
@@ -1,54 +1,8 @@
-import { resolve, dirname } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { Configuration } from 'webpack'
-import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin'
-import nodeExternals from 'webpack-node-externals'
-import { initConfig } from 'bistrio'
+import { generateServerWebpackConfig } from 'bistrio'
 import { config } from '../../config/index.ts'
-
+import { fileURLToPath } from 'node:url'
 const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
-const configFile = resolve(__dirname, '../../tsconfig.json')
-const bistrioConfig = initConfig(config)
-const structureConfig = bistrioConfig.structure
-
-const webpackConfig: Configuration = {
-  target: 'node20.10',
-  externalsPresets: { node: true },
-  externals: [nodeExternals()],
-  mode: 'development', // TODO: fix later
-  entry: resolve(__dirname, '../../server/boot.ts'),
-  output: {
-    path: resolve(__dirname, '../../dist/server'),
-    filename: 'boot.cjs',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: {
-          loader: 'ts-loader',
-          options: {
-            transpileOnly: true,
-            configFile,
-          },
-        },
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    plugins: [new TsconfigPathsPlugin({ configFile, extensions: ['.tsx', '.ts', '.js'] })],
-    extensions: ['', '.js', '.jsx', '.json', '.ts', '.tsx'],
-  },
-  cache: {
-    type: 'filesystem',
-    buildDependencies: {
-      config: [__filename],
-    },
-    cacheDirectory: resolve(structureConfig.cacheDir, 'webpack-server'),
-  },
-}
+const webpackConfig = generateServerWebpackConfig({ config, bundlerConfigPath: __filename })
 
 export default webpackConfig
---
+++ src/lib/webpack-server.ts
@@ -0,0 +1,67 @@
+import { resolve } from 'node:path'
+import createDebug from 'debug'
+import { Config, ConfigCustom, initConfig } from './config.js'
+import { GenerateProductionConfigFunc, generateWebpackConfig } from './webpack-base.js'
+import { TsconfigPathsPlugin } from 'tsconfig-paths-webpack-plugin'
+import nodeExternals from 'webpack-node-externals'
+
+const debug = createDebug('bistrio:webpack')
+
+const generateServerProductionConfig: GenerateProductionConfigFunc = ({ config, env, entry }) => {
+  const structureConfig = config.structure
+  const configFile = resolve(structureConfig.configDir, 'server', `tsconfig.server.${env}.json`)
+  if (env === 'development') {
+    debug('tsconfig: %s', configFile)
+  }
+
+  return {
+    target: 'node20.10',
+    externalsPresets: { node: true },
+    externals: [nodeExternals()],
+    mode: env,
+    entry,
+    output: {
+      path: resolve(structureConfig.distDir, 'server'),
+      filename: 'boot.cjs',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+              configFile,
+            },
+          },
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      plugins: [new TsconfigPathsPlugin({ configFile, extensions: ['.tsx', '.ts', '.js'] })],
+      extensions: ['', '.tsx', '.ts', '.js'],
+    },
+  }
+}
+
+export const generateServerWebpackConfig = ({
+  config: custom,
+  bundlerConfigPath,
+}: {
+  config: ConfigCustom
+  bundlerConfigPath: string
+}) => {
+  const config = initConfig(custom)
+  const entry = resolve(config.structure.serverDir, 'boot.ts')
+  const cacheName = 'webpack-server'
+
+  return generateWebpackConfig({
+    config,
+    bundlerConfigPath,
+    entry,
+    cacheName,
+    generateProductionConfig: generateServerProductionConfig,
+  })
+}
