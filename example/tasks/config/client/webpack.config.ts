91e0515b36c2e59092cb2df38f7a36254ad9c917
Masashi SATO <ms2.sato@gmail.com>
2023-11-29 11:35:59 +0900

    fix webpack config process optimize

--- example/tasks/config/client/webpack.config.ts
+++ example/tasks/config/client/webpack.config.ts
@@ -1,10 +1,10 @@
-import { generateWebpackConfig } from 'bistrio'
+import { generateClientWebpackConfig } from 'bistrio'
 import { config } from '../../config/index.ts'
 import { fileURLToPath } from 'node:url'
 
 const __filename = fileURLToPath(import.meta.url)
 
-const webpackConfig = generateWebpackConfig({
+const webpackConfig = generateClientWebpackConfig({
   config,
   bundlerConfigPath: __filename,
 })
