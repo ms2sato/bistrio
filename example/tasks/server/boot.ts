0072be6046930f34f5e2206aa3357aacc5467f70
Masashi SATO <ms2.sato@gmail.com>
2023-11-29 10:21:41 +0900

    rename server to boot

--- example/tasks/server/boot.ts
+++ example/tasks/server/boot.ts
@@ -1,12 +1,12 @@
 import { setup } from './app'
 import { support } from 'bistrio'
 
-async function main() {
+async function boot() {
   const app = await setup()
   support.boot(app)
 }
 
-main().catch((err) => {
+boot().catch((err) => {
   console.error(err)
   process.exit(1)
 })
