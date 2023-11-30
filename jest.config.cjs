d39b8ed69d83b79a650469548d8f2da7a7ccf2d5
Masashi SATO <ms2.sato@gmail.com>
2023-11-30 21:35:53 +0900

    fix jest to esm support

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,16 @@
 module.exports = {
   roots: ['./src'],
   testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/src/**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  extensionsToTreatAsEsm: ['.ts'], // @see https://kulshekhar.github.io/ts-jest/docs/guides/esm-support/
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
   },
 }
