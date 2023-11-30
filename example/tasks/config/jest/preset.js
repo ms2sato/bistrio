18303bd3ed5f698ded443d29d6e0d441a030989c
Masashi SATO <ms2.sato@gmail.com>
2023-11-30 21:53:42 +0900

    fix test

--- example/tasks/config/jest/preset.js
+++ example/tasks/config/jest/preset.js
@@ -1,4 +1,5 @@
-const ts_preset = require('ts-jest/jest-preset')
-const puppeteer_preset = require('jest-puppeteer/jest-preset')
+import ts_preset from 'ts-jest/jest-preset.js'
+import puppeteer_preset from 'jest-puppeteer/jest-preset.js'
+import esm_preset from 'ts-jest/presets/default-esm/jest-preset.js'
 
-module.exports = Object.assign(ts_preset, puppeteer_preset)
+export default Object.assign(ts_preset, puppeteer_preset, esm_preset)
