ac8e509126371c44739637f062fce5ede956524d
Masashi SATO <ms2.sato@gmail.com>
2023-11-30 21:53:42 +0900

    fix test

--- example/tasks/jest.config.js
+++ example/tasks/jest.config.js
@@ -1,16 +1,19 @@
-const config = {
+export default {
   verbose: true,
   roots: ['universal', 'server', '__tests__'],
   preset: './config/jest/preset.js',
+  moduleNameMapper: {
+    // @see https://kulshekhar.github.io/ts-jest/docs/guides/esm-support/
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
   transform: {
     '^.+\\.(ts|tsx)$': [
       'ts-jest',
       {
+        useESM: true,
         tsconfig: './config/tsconfig.test.json',
       },
     ],
   },
   testPathIgnorePatterns: ['<rootDir>/__tests__/support/'],
 }
-
-module.exports = config
