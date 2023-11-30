4a236616c478f25a7b9fc9815e4207c498f823d1
Masashi SATO <ms2.sato@gmail.com>
2023-11-30 22:05:20 +0900

    fix access url

--- example/tasks/__tests__/support/helper.ts
+++ example/tasks/__tests__/support/helper.ts
@@ -2,7 +2,7 @@ import { asURL } from '.'
 import { waitForAnyInnerText } from './request-spy'
 
 export const signIn = async (username: string, password: string) => {
-  await page.goto(asURL('/auth/login'))
+  await page.goto(asURL('auth/login'))
   await page.type('input[name=username]', username)
   await page.type('input[name=password]', password)
   await page.click('button[type="submit"]')
