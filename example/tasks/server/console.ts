7d7ef0b8ccf8b77f926dcfee675eecde863d4033
Masashi SATO <ms2.sato@gmail.com>
2023-12-09 14:41:47 +0900

    add routes() in npm run console

--- example/tasks/server/console.ts
+++ example/tasks/server/console.ts
@@ -1,8 +1,11 @@
 // for REPL
-import { support } from 'bistrio'
+import { initConfig, support } from 'bistrio'
 import { routes } from '../universal/routes/all'
+import { config } from '../config'
 import { serverRouterConfig } from './config/server'
 import { Middlewares } from '../universal/routes/middlewares'
+
+initConfig(config)
 
 support.loadResources<Middlewares>(serverRouterConfig(), routes).catch((err: Error) => {
   console.error(`console error: ${err.message}`)
