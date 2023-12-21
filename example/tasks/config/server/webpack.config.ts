6207964aad861981e7e1d1644eee8dd5f447d5d0
Masashi SATO <ms2.sato@gmail.com>
2023-12-21 21:45:12 +0900

    dist esm

--- example/tasks/config/server/webpack.config.ts
+++ example/tasks/config/server/webpack.config.ts
@@ -5,4 +5,11 @@ const __filename = fileURLToPath(import.meta.url)
 
 const webpackConfig = generateServerWebpackConfig({ config, bundlerConfigPath: __filename })
 
+webpackConfig.experiments = {
+  outputModule: true,
+}
+webpackConfig.output!.module = true
+webpackConfig.output!.filename = '[name].js'
+webpackConfig.output!.library = { type: 'module' }
+
 export default webpackConfig
