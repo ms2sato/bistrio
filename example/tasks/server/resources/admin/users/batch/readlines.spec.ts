03805bc82505d9d594a5fe60cda2818559dd28b4
Masashi SATO <ms2.sato@gmail.com>
2024-01-13 10:00:03 +0900

    add bufferSize to readLines

--- example/tasks/server/resources/admin/users/batch/readlines.spec.ts
+++ example/tasks/server/resources/admin/users/batch/readlines.spec.ts
@@ -1,34 +1,57 @@
 import { readLines } from './readlines'
 
 describe('readLines', () => {
-  it('reads lines correctly', async () => {
-    // Mock the File class and its stream method
-    const mockStream = {
-      pipeThrough: jest.fn().mockReturnThis(),
-      getReader: jest.fn().mockReturnValue({
-        read: jest
-          .fn()
-          .mockResolvedValueOnce({ value: 'line1\nline2\nline3\nli', done: false })
-          .mockResolvedValueOnce({ value: 'ne4\nline5', done: true }),
-      }),
-    }
-    const mockFile = { stream: jest.fn().mockReturnValue(mockStream) } as unknown as File
+  describe('bufferSize = 1', () => {
+    it('reads lines correctly', async () => {
+      const mockStream = {
+        pipeThrough: jest.fn().mockReturnThis(),
+        getReader: jest.fn().mockReturnValue({
+          read: jest
+            .fn()
+            .mockResolvedValueOnce({ value: 'line1\nline2\nline3\nli', done: false })
+            .mockResolvedValueOnce({ value: 'ne4\nline5', done: true }),
+        }),
+      }
+      const mockFile = { stream: jest.fn().mockReturnValue(mockStream) } as unknown as File
 
-    // Define the callback function
-    const callback = jest.fn().mockResolvedValue('processed')
+      const callback = jest.fn().mockResolvedValue('processed')
 
-    // Call the function
-    const result = await readLines(mockFile, callback)
+      const result = await readLines(mockFile, callback)
 
-    // Check that the callback was called with each line
-    expect(callback).toHaveBeenCalledTimes(5)
-    expect(callback).toHaveBeenNthCalledWith(1, ['line1'])
-    expect(callback).toHaveBeenNthCalledWith(2, ['line2'])
-    expect(callback).toHaveBeenNthCalledWith(3, ['line3'])
-    expect(callback).toHaveBeenNthCalledWith(4, ['line4'])
-    expect(callback).toHaveBeenNthCalledWith(5, ['line5'])
+      expect(callback).toHaveBeenCalledTimes(5)
+      expect(callback).toHaveBeenNthCalledWith(1, ['line1'])
+      expect(callback).toHaveBeenNthCalledWith(2, ['line2'])
+      expect(callback).toHaveBeenNthCalledWith(3, ['line3'])
+      expect(callback).toHaveBeenNthCalledWith(4, ['line4'])
+      expect(callback).toHaveBeenNthCalledWith(5, ['line5'])
 
-    // Check the result
-    expect(result).toEqual({ lines: 5, results: ['processed', 'processed', 'processed', 'processed', 'processed'] })
+      expect(result).toEqual({ lines: 5, results: ['processed', 'processed', 'processed', 'processed', 'processed'] })
+    })
+  })
+
+  describe('multi bufferSize', () => {
+    it('reads lines correctly', async () => {
+      const mockStream = {
+        pipeThrough: jest.fn().mockReturnThis(),
+        getReader: jest.fn().mockReturnValue({
+          read: jest
+            .fn()
+            .mockResolvedValueOnce({ value: 'line1\nline2\nline3\nli', done: false })
+            .mockResolvedValueOnce({ value: 'ne4\nline5', done: true }),
+        }),
+      }
+      const mockFile = { stream: jest.fn().mockReturnValue(mockStream) } as unknown as File
+
+      const callback = jest.fn().mockResolvedValue('processed')
+
+      const result = await readLines(mockFile, callback, 2)
+
+      expect(callback).toHaveBeenCalledTimes(3)
+      expect(callback).toHaveBeenNthCalledWith(1, ['line1', 'line2'])
+      expect(callback).toHaveBeenNthCalledWith(2, ['line3', 'line4'])
+      expect(callback).toHaveBeenNthCalledWith(3, ['line5'])
+
+      expect(result).toEqual({ lines: 5, results: ['processed', 'processed', 'processed'] })
+    })
   })
 })
--- example/tasks/server/resources/admin/users/batch/readlines.ts
+++ example/tasks/server/resources/admin/users/batch/readlines.ts
@@ -5,27 +5,23 @@ export type ReadLineResult<R> = {
 
 export type ReadLineCallback<R> = (line: string[]) => Promise<R>
 
-export const readLines = async <R>(file: File, callback: ReadLineCallback<R>): Promise<ReadLineResult<R>> => {
+export const readLines = async <R>(
+  file: File,
+  callback: ReadLineCallback<R>,
+  bufferSize = 1,
+): Promise<ReadLineResult<R>> => {
   const readableStream = file.stream()
-
-  // ReadableStream をテキストに変換
   const textStream = readableStream.pipeThrough(new TextDecoderStream())
-
-  // ラインリーダーを作成
   const reader = textStream.getReader()
 
   const results: R[] = []
-
-  // ラインバッファ
   const lines: string[] = []
   let incompleteLine: string = ''
   let chunk
   let readerDone
   let lineCount = 0
 
-  // ストリームを読み込み、行ごとに処理
   do {
-    // 次のチャンクを読み込む
     ;({ value: chunk, done: readerDone } = await reader.read())
     chunk = chunk ? chunk : ''
 
@@ -34,7 +30,7 @@ export const readLines = async <R>(file: File, callback: ReadLineCallback<R>): P
     const newLineIncomplete = !chunk.endsWith('\n')
 
     if (newLineIncomplete) {
-      incompleteLine = chunkLines.pop() || '' // 最後の行は完全ではない可能性がある
+      incompleteLine = chunkLines.pop() || ''
     } else {
       incompleteLine = ''
     }
@@ -42,20 +38,20 @@ export const readLines = async <R>(file: File, callback: ReadLineCallback<R>): P
     lines.push(...chunkLines)
     lineCount += chunkLines.length
 
-    for (const l of lines) {
-      // ここで各行に対する処理を行う
-      //console.log(l);
-      results.push(await callback([l]))
+    while (lines.length >= bufferSize) {
+      const buffer = lines.splice(0, bufferSize)
+      results.push(await callback(buffer))
     }
-
-    lines.length = 0
   } while (!readerDone)
 
-  // 最後の行を処理（もし完了していない場合）
   if (incompleteLine) {
     lineCount++
-    //console.log(line);
-    results.push(await callback([incompleteLine]))
+    lines.push(incompleteLine)
+  }
+
+  while (lines.length > 0) {
+    const buffer = lines.splice(0, bufferSize)
+    results.push(await callback(buffer))
   }
 
   return Promise.resolve({ lines: lineCount, results })
