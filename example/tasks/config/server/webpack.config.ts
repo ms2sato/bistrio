556b04f5d6ed7816650c6f86e2aab8c76d8d8022
Masashi SATO <ms2.sato@gmail.com>
2024-02-10 10:19:05 +0900

    revert unused modify

--- example/tasks/config/server/webpack.config.ts
+++ example/tasks/config/server/webpack.config.ts
@@ -1,19 +1,8 @@
 import { generateServerWebpackConfig } from 'bistrio'
 import { config } from '../../config'
 import { fileURLToPath } from 'node:url'
-import webpack from 'webpack'
 const __filename = fileURLToPath(import.meta.url)
 
 const webpackConfig = generateServerWebpackConfig({ config, bundlerConfigPath: __filename })
-// webpackConfig.cache = false
-// //webpackConfig.output.chunkFilename = '[name].[contenthash].js'
-// // webpackConfig.output = {
-// //   ...webpackConfig.output!,
-// //   clean: true,
-// //   filename: '[name].js?t=[contenthash]',
-// // }
-// webpackConfig.plugins = [new webpack.HotModuleReplacementPlugin()]
-
-// console.log('$$$ Server webpackConfig', JSON.stringify(webpackConfig, null, 2))
 
 export default webpackConfig
